5f832422059afc8c75fa1b5a7e75322c676c2676
MohamedAbdullahi2 <118404373+MohamedAbdullahi2@users.noreply.github.com>
2023-04-12 18:38:19 -0400

    server js code updated

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
+
+// Import the Express library and the database connection module
 const express = require('express');
 const db = require('./config/connection');
-// const mongoose = require('mongoose');
+
+// Import the application routes
 const routes = require('./routes')
 
+// Create a new Express app
 const app = express();
+
+// Set the port to listen to, either the one specified in the environment or 3001
 const PORT = process.env.PORT || 3001;
 
+// Middleware to parse request bodies as JSON
 app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+// Use the application routes
 app.use(routes);
 
+// Listen for the database connection to open, then start the server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on localhost:${PORT}!`);
   });
 });
-
-
